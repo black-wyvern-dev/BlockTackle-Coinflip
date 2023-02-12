d7d3e6ddb9c8007f3dd49516449eb28e3180a621
albertomira1114 <alberto.mira@blocktackle.io>
2023-02-13 00:25:47 +0900

    test: GA-integration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
 
@@ -7,19 +7,23 @@ import './assets/scss/style.css';
 import { Wallets } from './components/wallet';
 import { context } from './contexts/context';
 
-const TRACKING_ID = "G-V9J2GN1NKX";
-ReactGA.initialize(TRACKING_ID);
-
-const history = createBrowserHistory();
-// Initialize google analytics page view tracking
-history.listen(location => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
 
 export default function App() {
   const [token, setToken] = useState(null);
   const [chipAmount, setChipAmount] = useState(null);
+
+  const TRACKING_ID = "G-V9J2GN1NKX";
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    console.log('==> Google Analytics are initialized', TRACKING_ID);
+    const history = createBrowserHistory();
+    // Initialize google analytics page view tracking
+    history.listen(location => {
+      ReactGA.set({ page: location.pathname }); // Update the user's current page
+      ReactGA.pageview(location.pathname); // Record a pageview for the given page
+    });
+  }, []);
+
   return (
     <context.Provider value={{ token, setToken, chipAmount, setChipAmount }}>
       <Wallets>
